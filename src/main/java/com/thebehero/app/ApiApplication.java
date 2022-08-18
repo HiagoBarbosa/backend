9fd587fca906630fb2139b1c5989e16df3c0b0c5
HiagoBarbosa <hiaaugusto@hotmail.com>
2022-08-18 20:51:16 -0300

    ajuste descricao

---
+++ src/main/java/com/thebehero/app/ApiApplication.java
@@ -0,0 +1,26 @@
+package com.thebehero.app;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+@SpringBootApplication
+public class ApiApplication {
+	
+    @Autowired
+    private ObjectMapper objectMapper;
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+	
+    @PostConstruct
+    public void setUp() {
+        this.objectMapper.registerModule(new JavaTimeModule());
+    }
+}
---
+++ src/main/java/com/thebehero/descricao/Descricoes.java
@@ -0,0 +1,7 @@
+package com.thebehero.descricao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Descricoes extends JpaRepository<Descricoes, Long>{
+
+}
