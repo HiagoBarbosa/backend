3024a5882de712f9f00a2dab1625431128c06e9c
HiagoBarbosa <hiaaugusto@hotmail.com>
2022-08-20 17:40:07 -0300

    ong

--- src/main/java/com/thebehero/caso/Caso.java
+++ src/main/java/com/thebehero/caso/Caso.java
@@ -3,9 +3,6 @@ package com.thebehero.caso;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.annotations.GenericGenerator;
