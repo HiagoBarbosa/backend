0648bd93801f741bb73ff3238cc37b47f49d09f5
HiagoBarbosa <hiaaugusto@hotmail.com>
2022-08-22 22:41:59 -0300

    ManyToOne ong

--- src/main/java/com/thebehero/ong/Ong.java
+++ src/main/java/com/thebehero/ong/Ong.java
@@ -3,6 +3,7 @@ package com.thebehero.ong;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.annotations.GenericGenerator;
@@ -141,7 +142,7 @@ public class Ong {
 		this.image = image;
 	}
 
-	
+	@ManyToOne
 	private Caso caso;
 	  
 		@Override
