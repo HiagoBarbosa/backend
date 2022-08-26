3da8abaf77827a1b355e10f00e10bba8417118dc
HiagoBarbosa <hiaaugusto@hotmail.com>
2022-08-25 22:28:57 -0300

    aqui esta funcionando

--- src/main/java/com/thebehero/caso/CasosRepository.java
+++
@@ -1,8 +0,0 @@
-package com.thebehero.caso;
-
-import org.springframework.data.jpa.repository.*;
-
-public interface CasosRepository extends JpaRepository<CasoEntidade, Long> {
-	public CasoEntidade findbyinserir(Long id);
-	public CasoEntidade findAll(String nome); 
-}
