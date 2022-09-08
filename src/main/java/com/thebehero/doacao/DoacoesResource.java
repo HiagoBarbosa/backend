be2d16f96dda1015fe9239363389f72b89032038
HiagoBarbosa <hiaaugusto@hotmail.com>
2022-09-07 23:22:56 -0300

    ajuste doacao

--- src/main/java/com/thebehero/doacao/DoacoesResource.java
+++ src/main/java/com/thebehero/doacao/DoacoesResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/doacoes")
 public class DoacoesResource {
+	@Autowired
 	private Doacoes doacoes;
 
 	@GetMapping
