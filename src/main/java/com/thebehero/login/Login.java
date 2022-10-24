8e111dee9c1e0ae4efd1da866728ce6d0d6d21e3
HiagoBarbosa <hiaaugusto@hotmail.com>
2022-10-23 23:26:59 -0300

    login

--- src/main/java/com/thebehero/login/Login.java
+++ src/main/java/com/thebehero/login/Login.java
@@ -1,13 +1,27 @@
 package com.thebehero.login;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 public class Login {
+	
+	@Id
+	@GeneratedValue(generator = "increment")
+	@GenericGenerator(name = "increment", strategy = "increment")
+	private Long id;
 	private String emailId;
 	private String password;
 	
-	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
 	public String getEmailId() {
 		return emailId;
 	}
