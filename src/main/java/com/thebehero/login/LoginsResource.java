c57f3781daa997c457b6cc2d67b0ef327702a920
HiagoBarbosa <hiaaugusto@hotmail.com>
2023-02-23 16:41:19 -0300

    ajuste besta para ver se funciona

--- src/main/java/com/thebehero/login/LoginsResource.java
+++ src/main/java/com/thebehero/login/LoginsResource.java
@@ -1,5 +1,6 @@
 package com.thebehero.login;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,15 +8,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.thebehero.ong.Ong;
+import com.thebehero.ong.Ongs;
 
 @RestController
 @RequestMapping("/login")
 public class LoginsResource {
-
+	@Autowired
+	private Ongs ongs;
 	
 	@PostMapping
 	public ResponseEntity<Ong> login(@RequestBody Login login){	
-		Ong ong = new Ong();
+	
+		Ong ong = ongs.getOne(null);
 		if(login.getEmailId().equals(ong.getEmail()) & login.getPassword().equals(ong.getPassword())) {	
 			return ResponseEntity.ok().body(ong);
 		}
