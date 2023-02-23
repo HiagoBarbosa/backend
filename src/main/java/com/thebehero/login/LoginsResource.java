2b979da02209f099ce1c0270e88b701e5de29b53
HiagoBarbosa <hiaaugusto@hotmail.com>
2023-02-23 17:45:53 -0300

    retornou ao inicio

--- src/main/java/com/thebehero/login/LoginsResource.java
+++ src/main/java/com/thebehero/login/LoginsResource.java
@@ -1,8 +1,5 @@
 package com.thebehero.login;
 
-import javax.validation.Valid;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,24 +7,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.thebehero.ong.Ong;
-import com.thebehero.ong.Ongs;
 
 @RestController
 @RequestMapping("/login")
 public class LoginsResource {
-	@Autowired
-	private Ongs ongs;
+
 	
 	@PostMapping
-	public ResponseEntity<Ong> login(@Valid @RequestBody Login login){	
+	public ResponseEntity<Ong> login(@RequestBody Login login){	
 		Ong ong = new Ong();
 		if(login.getEmailId().equals(ong.getEmail()) & login.getPassword().equals(ong.getPassword())) {	
-			return (ResponseEntity<Ong>) ResponseEntity.ok().header(null, null);
+			return ResponseEntity.ok().body(ong);
 		}
 		else {
-			return (ResponseEntity<Ong>) ResponseEntity.ok().header(null, null);
-			//return ResponseEntity.ok().body(ong);
-			//return ResponseEntity.status(401).build();
+			return ResponseEntity.status(401).build();
 		}
 	}
 
